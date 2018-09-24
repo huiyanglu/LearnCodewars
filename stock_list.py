c56197510685c9edb54b52e1051cee30c4b1588a
huiyanglu <43104281+huiyanglu@users.noreply.github.com>
2018-09-25 01:19:18 +0800

    Update stock_list.py

--- stock_list.py
+++ stock_list.py
@@ -1,19 +1,18 @@
 def stock_list(listOfArt, listOfCat):
-    if len(listOfCat) == 0 or len(listOfCat) == 0:
+    if listOfArt == [] or listOfCat == []:
         return ''
-    ans = [int(i) for i in list(str(0)*len(listOfCat))]
-    for i in listOfArt:
-        i = i.split()
-        if i[0][0] in listOfCat:
-            ans[listOfCat.index(i[0][0])] += int(i[1])
-    if ans.count(0) == len(ans):
-        return ''
-    ans_arr = []
-    i = 0
-    while i < len(listOfCat):
-        ans_arr.append('(' + listOfCat[i] + ' : ' + str(ans[i]) + ')')
-        i += 1
-    return ' - '.join(ans_arr)
+    cnt = 0
+    rst = ''
+    for i in listOfCat:
+        for j in listOfArt:
+            if i == j[0]:
+                sum = ''
+                for m in j:
+                    if m.isdigit():
+                        sum += m
+                cnt += int(sum)
+        rst += '(' + i + ' : ' + str(cnt) + ') - '
+    return rst[:-3]
 
 b = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"]
 c = ["A", "B"]
