868ef1ae4bd1d82007eeffdaa4d1d0d46c9e6059
huiyanglu <43104281+huiyanglu@users.noreply.github.com>
2018-10-01 01:02:56 +0800

    Update beggars.py

--- beggars.py
+++ beggars.py
@@ -1,1 +1,8 @@
-def beggars(values,n):
+def beggars(a,n):
+    res = [ 0 for i in range(n)]
+    for j in range(0,len(a)):
+        if n != 0:
+            res[j%n] += a[j]
+        else:
+            return []
+    return res
