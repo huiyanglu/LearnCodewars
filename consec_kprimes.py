fdafd3352a1fddb6c574e622464ef40da8b6ec42
huiyanglu <43104281+huiyanglu@users.noreply.github.com>
2018-10-06 20:10:56 +0800

    Update consec_kprimes.py

--- consec_kprimes.py
+++ consec_kprimes.py
@@ -1,1 +1,24 @@
+def factoring(n):
+    primefac = []
+    d = 2
+    while d * d <= n:
+        while (n % d) == 0:
+            primefac.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+        primefac.append(n)
+    return len(primefac)
 
+
+def consec_kprimes(k, arr):
+    x = []
+    for i in range(0, len(arr)-1):
+        b = factoring(arr[i])
+        b2 = factoring(arr[i+1])
+        if b == k and b2 == k :
+            x.append(b)
+            i += 1
+        else:
+            i += 1
+    return len(x)
