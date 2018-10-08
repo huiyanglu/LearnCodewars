a1c6b89ba96a3bd5cbc6c90774080b1bf402157c
huiyanglu <43104281+huiyanglu@users.noreply.github.com>
2018-10-08 21:07:14 +0800

    Update smallest.py

--- smallest.py
+++ smallest.py
@@ -1,1 +1,12 @@
 def smallest(n):
+    n2 = [i for i in str(n)]
+    x = []
+    for i,n in enumerate(n2):
+        ns = list(n2)
+        ns.pop(i)
+        for j in range(len(n2)):
+            x2 = list(ns)
+            x2.insert(j,n)
+            x2 = int(''.join(x2))
+            x.append([x2,i,j])
+    return min(x,key = lambda x:x[0])
