2f0642c61381c6c0488aeaa549e85170b23e11bf
huiyanglu <43104281+huiyanglu@users.noreply.github.com>
2018-10-15 12:03:21 +0800

    Update isPP.py

--- isPP.py
+++ isPP.py
@@ -1,10 +1,8 @@
-from math import ceil, log, sqrt
- 
+from math import log, sqrt
+
 def isPP(n):
-    for m in range(2, int(sqrt(n)) + 1):
-        k = int(round(log(n, m)))
-        if m ** k == n:
-            return [m, k]
-    return None
- 
- 
+    for m in range(2, int(sqrt(n)) + 1):
+        k = int(round(log(n, m)))
+        if m ** k == n:
+            return [m, k]
+    return None
