273bd0058b76604aeac80e4108132a062faeea35
huiyang lu <luchocoice@gmail.com>
2018-12-20 02:03:52 +0800

    Update 4kyu_differentiate.py

--- 4 kyu/4kyu_differentiate.py
+++ 4 kyu/4kyu_differentiate.py
@@ -1,24 +1,19 @@
 import re
-import numpy as np
 
 def differentiate(equation,point):
     res = re.split(r'(\+|-)',equation)
     res1 = res[1:]
     res1 = [res[0]]+["".join(i).strip() for i in zip(res1[0::2],res1[1::2])]
-    if res1[0] == '':
-        res1.pop(0)
-    no_x = [i for i in res1 if 'x' not in i]
+    #if res1[0] == '':
+        #res1.pop(0)
     if '^' in res1[0]:
         zhishuMax = res1[0][res1[0].index('^')+1:]
         l = int(zhishuMax)+1
     else:
         l = len(res1)
-    #if no_x == []:
-        #l = len(res1)+1
-    #else:
-        #l=len(res1)
     lst = [0]*l
-    for i in range(0,l-1):
+    len_lst = len(res1)
+    for i in range(0,len_lst):
         if 'x' not in res1[i]:
             lst[-1] = int(res1[i])
         else:
@@ -34,10 +29,16 @@ def differentiate(equation,point):
                 lst[l - int(zhishu) - 1] = -1
             else:
                 lst[l - int(zhishu) - 1] = int(res1[i][:idx_x])
-    a = np.array(lst)
-    p = np.poly1d(a)
-    deri = p.deriv()
-    return deri(point)
+    sum = 0
+    for i in range(0,len(lst)):
+        if lst[i]==0:
+            pass
+        elif l-i-2>=0 and lst[i]!=0:
+            sum+=lst[i]*(l-i-1)*(point**(l-i-2))
+        else:
+            break
+    return sum
 
-txt = '21x^4+3x^3'
-print(differentiate(txt,414))
+print(differentiate('48x^6+36x^5-18x^4+x^3+76x^2+36x+1',4968))
+
+
