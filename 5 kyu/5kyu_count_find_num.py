99fc9a4245357ed2f1cdef0095c246efefdad961
huiyang lu <luchocoice@gmail.com>
2019-01-08 23:58:11 +0800

    Update 5kyu_count_find_num.py

--- 5 kyu/5kyu_count_find_num.py
+++ 5 kyu/5kyu_count_find_num.py
@@ -44,28 +44,22 @@ number of tests = 200
 Enjoy!
 
 """
-import itertools
 from functools import reduce
 
 def count_find_num(primesL,limit):
-    prod_base = reduce(lambda x,y:x*y,primesL)
-    rst = [1]+[i for i in primesL]
-    count = 0
-    for num in range(2,25):
-        each_comb = [i for i in itertools.combinations_with_replacement(primesL, num)]
-        for j in range(0,len(each_comb)):
-            changetoint = [int(i) for i in each_comb[j]]
-            prod_each_comb = reduce(lambda x,y:x*y,changetoint)
-            rst.append(prod_each_comb)
-    sorted_prod_lst = sorted(rst)
-    for num2 in range(0,len(sorted_prod_lst)):
-        final_rst = sorted_prod_lst[num2]*prod_base
-        if final_rst<=limit:
-            count+=1
-        elif limit<prod_base:
-            return []
-        else:
-            return [count,prod_base*(sorted_prod_lst[count-1])]
+    base = reduce(lambda x, y: x * y, primesL)
+    rst1 = [base]
+    if base>limit:
+        return []
+    for each in primesL:
+        for num in rst1:
+            num = num * each
+            if num > limit:# 如果排列后的rst1的num已经大于limit了，就不继续往下遍历直接退出循环进入下一项了
+                break
+            else:
+                while num not in rst1 and num<=limit:
+                    rst1 = sorted(rst1+[num])#加入列表的数字是未排序的，及时排序可以减少遍历次数
+                    num*=each
+    return [len(rst1),max(rst1)]
 
-print(count_find_num([2, 181, 277, 449],183207235273926))
-
+print(count_find_num([2,3,5],500))
