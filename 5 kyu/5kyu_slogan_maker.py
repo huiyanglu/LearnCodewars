656d36e3519d6b5d2b6fd7457de9ea2dc755c900
huiyang lu <luchocoice@gmail.com>
2019-01-22 00:56:29 +0800

    update 5kyu_slogan_maker.py

--- 5 kyu/5kyu_slogan_maker.py
+++ 5 kyu/5kyu_slogan_maker.py
@@ -36,10 +36,5 @@ def slogan_maker(a):
     x = []
     result = list(itertools.permutations(a2, len(a2))) # 字符串排列组合考虑顺序
     for i in range(0,len(result)):
-        x.append(join1(result[i]))
-        i+=1
-    return x
-
-def join1(x): # 字符串合并join
-    x1 = list(x)
-    return ' '.join(x1)
+        x.append(' '.join(result[i]))
+    return x 
