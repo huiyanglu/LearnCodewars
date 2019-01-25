4fe2861226a389b840f93d42743dac9ad6d3c001
huiyang lu <luchocoice@gmail.com>
2019-01-25 23:49:00 +0800

    Update 6kyu_thirt.py

--- 6 kyu/6kyu_thirt.py
+++ 6 kyu/6kyu_thirt.py
@@ -37,25 +37,16 @@ thirt(321) calculates 48, 48 and returns 48
 
 """
 
+import itertools as it
+
 def thirt(n):
-    x = []
-    for i in range(0,len(str(n))+1):
-        a = (10**i)%13
-        x.append(a)
-        i+=1
+    pattern = it.cycle([1, 10, 9, 12, 3, 4])
     l1 = [int(i) for i in str(n)]
     l1.reverse()
-    prod = [a * b for a, b in zip(l1, x)]
-    s1 = sum(prod)
-    for j in range(0,100):
-        l2 = [int(i) for i in str(s1)]
-        l2.reverse() #数列反序
-        prod2 = [a * b for a, b in zip(l2, x)] #数组相乘
-        s2 = sum(prod2)
-        if s1 != s2:
-            j+=1
-            s1 = s2
-            s2 = 0
-        else:
-            return s1
+    prod_sum = sum([a * b for a, b in zip(l1, pattern)])
+    if n == prod_sum:
+        return prod_sum
+    return thirt(prod_sum)
+
+print(thirt(1234567))
 
