e98cdabb3cc9c1b48b091c3471a88b0cc0486a75
huiyang lu <luchocoice@gmail.com>
2019-01-28 00:07:22 +0800

    Update 5kyu_convertFracts.py

--- 5 kyu/5kyu_convertFracts.py
+++ 5 kyu/5kyu_convertFracts.py
@@ -43,25 +43,15 @@ output is then "6 12 4 12 3 12"
 
 
 from functools import reduce
+from math import gcd
 
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-def lcm(a, b):
-    return a * b // gcd(a, b)
+def get_lcm(lst):
+    return reduce(lambda a,b: a * b // gcd(a, b),lst)
 
 def convertFracts(lst):
-    denoms = [i[1] for i in lst] # denominator分母
-    nums = [i[0] for i in lst] # numerator分子
-    lsttt = denoms + nums # [4, 6, 8, 2, 2, 2]
-    _gcd = reduce(gcd,lsttt) # 2
-    denoms2 = [i[1]//_gcd for i in lst] # 化简后的分母
-    nums2 = [i[0]//_gcd for i in lst] # 化简后的分子
-    _lcm = reduce(lcm,denoms2) # lcm是函数
-    multi = [_lcm//i for i in denoms2]
-    return [[i*m,_lcm] for i,m in zip(nums2,multi)]
+    my_gcd = reduce(gcd,[i[0]+i[1] for i in lst])
+    my_lcm = get_lcm([i[1]//my_gcd for i in lst])
+    return [[i[0]*my_lcm//i[1],my_lcm] for i in lst]
 
-a = [[2,4], [2, 6], [2, 8]] 
+a = [[2,4], [2, 6], [2, 8]]
 print(convertFracts(a))
