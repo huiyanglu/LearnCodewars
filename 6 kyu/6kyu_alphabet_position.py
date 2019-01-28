fbb12ff4a16b97b50f8bac8552f09e6b5865c088
huiyang lu <luchocoice@gmail.com>
2019-01-29 01:28:18 +0800

    update 20190129

--- 6 kyu/6kyu_alphabet_position.py
+++ 6 kyu/6kyu_alphabet_position.py
@@ -14,16 +14,7 @@ alphabet_position("The sunset sets at twelve o' clock.")
 Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 
 """
-
 def alphabet_position(text):
-    lst = []
-    court = 0
-    txt = text.lower()
-    for i in range(0,len(text)):
-        if txt[i].isalpha():
-            b = ord(txt[i])-96
-            lst.append(b)
-            i+=1
-        else:
-            i+=1
-    return ' '.join(str(x) for x in lst)
+    return ' '.join([str(ord(i)-96) for i in text.lower() if i.isalpha()])
+
+
--- 6 kyu/6kyu_stock_list.py
+++ 6 kyu/6kyu_stock_list.py
@@ -34,18 +34,15 @@ In the result codes and their values are in the same order as in M.
 def stock_list(listOfArt, listOfCat):
     if listOfArt == [] or listOfCat == []:
         return ''
-    cnt = 0
-    rst = ''
+    rst1 = []
     for i in listOfCat:
+        cnt = 0
         for j in listOfArt:
             if i == j[0]:
-                sum = ''
-                for m in j:
-                    if m.isdigit():
-                        sum += m
-                cnt += int(sum)
-        rst += '(' + i + ' : ' + str(cnt) + ') - '
-    return rst[:-3]
+                j2 = j.split(' ')
+                cnt += int(j2[-1])
+        rst1.append('({} : {})'.format(i,cnt))
+    return ' - '.join(rst1[i] for i in range(len(rst1)))
 
 b = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"]
 c = ["A", "B"]
