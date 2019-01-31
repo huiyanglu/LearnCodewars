90ecdfaef3e4f97798f69e147aa9b643dcd67c82
huiyang lu <luchocoice@gmail.com>
2019-01-31 23:54:05 +0800

    update rot13 and stat_CAA

--- 5 kyu/5kyu_rot13.py
+++ 5 kyu/5kyu_rot13.py
@@ -11,34 +11,23 @@ Please note that using "encode" in Python is considered cheating.
 
 """
 # -*- coding:utf-8 -*-
-
+import re
 import string
 
-def rot13(s):
-    uppd = {}
-    lowd = {}
+regex = re.compile('[a-z]{1}', re.IGNORECASE)
+
+def replace(match):
     upletters = string.ascii_uppercase  # 大写
     lowletters = string.ascii_lowercase  # 小写
-    for i in range(0, len(lowletters)):
-        if i < 13:
-            lowd[lowletters[i]] = lowletters[i + 13]
-        else:
-            lowd[lowletters[i]] = lowletters[i - 13]
 
-    for i in range(0, len(upletters)):
-        if i < 13:
-            lowd[upletters[i]] = upletters[i + 13]
-        else:
-            lowd[upletters[i]] = upletters[i - 13]
-    lst = []
-    for m in s:
-        if m in lowd:
-            lst.append(lowd[m])
-        elif m in uppd:
-            lst.append(uppd[m])
-        else:
-            lst.append(m)
-    return ''.join(lst)
-print(rot13('about'))
+    word = match.group(0)
+    if word in lowletters:
+        return lowletters[lowletters.index(word[0])+13 if lowletters.index(word[0])<13 else lowletters.index(word[0])-13]
+    else:
+        return upletters[
+            upletters.index(word[0]) + 13 if upletters.index(word[0]) < 13 else upletters.index(word[0]) - 13]
 
+def rot13(s):
+    return regex.sub(replace, s)
 
+print(rot13('aboutYOU'))
--- 6 kyu/6kyu_stat_CAA.py
+++ 6 kyu/6kyu_stat_CAA.py
@@ -34,12 +34,23 @@ if a result in seconds is ab.xy... it will be given truncated as ab.
 if the given string is "" you will return ""
 
 """
+def changetoHMS(x):
+    h1 = int(x // 3600)
+    m1 = int((x - 3600 * h1) // 60)
+    s1 = int(x - 3600 * h1 - 60 * m1)
+    return ' {h1:02d}|{m1:02d}|{s1:02d}'.format(**locals())
 
-def change(x):
-    h1 = x // 3600
-    m1 = (x - 3600 * h1) // 60
-    s1 = x - 3600 * h1 - 60 * m1
-    return h1,m1,s1
+def changetoTime(str):
+    b1 = str.split('|')
+    return int(b1[0]) * 3600 + int(b1[1]) * 60 + int(b1[2])
+
+def getMedium(lst):
+    l = len(lst)
+    lst.sort()
+    if l % 2 == 0:
+        return (lst[l // 2] + lst[l // 2 - 1]) / 2
+    else:
+        return lst[l // 2]
 
 def stat(strg):
     if strg =='':
@@ -47,27 +58,11 @@ def stat(strg):
     else:
         s = strg.split(',')
         lst = []
-        sum = 0
-        l = len(s)
-        try:
-            int('')
-        except ValueError:
-            pass
-        for i in range(0,l):
-            b1 = s[i].split('|')  # 数列的第一个数将数字分隔开
-            c1 = int(b1[0]) * 3600 + int(b1[1]) * 60 + int(b1[2])
-            lst.append(c1)
-            sum += lst[i]
-        avg = sum//l
-        rge = max(lst)-min(lst)
-        if l%2 == 0:
-            lst.sort()
-            med = (lst[l//2]+lst[l//2-1])/2
-        else:
-            lst.sort()
-            med = lst[l//2]
-        avgr = change(avg)
-        rger = change(rge)
-        medr = change(med)
-        return "Range: %02d|%02d|%02d "%rger+"Average: %02d|%02d|%02d "%avgr+"Median: %02d|%02d|%02d"%medr
+        for i in range(0,len(s)):
+            lst.append(changetoTime(s[i]))
+        avgr = changetoHMS(sum(lst)//len(lst))
+        rger = changetoHMS(max(lst)-min(lst))
+        medr = changetoHMS(getMedium(lst))
+        return 'Range:{rger} Average:{avgr} Median:{medr}'.format(**locals())
 
+print(stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"))
