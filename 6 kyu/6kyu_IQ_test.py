5a39bd19c5df327517a248a3db29043d97597b23
huiyang lu <luchocoice@gmail.com>
2019-02-01 23:53:32 +0800

    Update 6kyu_IQ_test.py

--- 6 kyu/6kyu_IQ_test.py
+++ 6 kyu/6kyu_IQ_test.py
@@ -15,23 +15,13 @@ iq_test("1 2 1 1") => 2 // Second number is even, while the rest of the numbers 
 """
 
 def iq_test(lines):
-    even = []
-    odd = []
     integers = lines.split(' ')
-    #分割每个数字，分别加引号，成为一个数组[]
-    print(integers)
-    for num in integers:
-        if int(num) % 2 == 0:
-            even.append(num)
-        else:
-            odd.append(num)
-    if len(even)>1 and len(odd)==1:
-        spec = odd[0]
-        return integers.index(spec)+1
-        #index生成该数字的索引，即在数列中的位置，从0开始
-
-    elif len(odd)>1 and len(even)==1:
+    even = [num for num in integers if int(num)%2==0]
+    if len(even)==1:
         spec = even[0]
         return integers.index(spec)+1
+        # index生成该数字的索引，即在数列中的位置，从0开始
+    else:
+        spec = [num for num in integers if int(num)%2!=0]
+        return integers.index(spec[0])+1
 
-print(iq_test('1 3 5 7 8'))
