4fcbe01cfd00b493f33ce927f2168d01d1248e38
huiyang lu <luchocoice@gmail.com>
2019-04-10 23:25:04 +0800

    Create 9.palindrome-number.py

---
+++ LeetCode/9.palindrome-number.py
@@ -0,0 +1,77 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+# https://leetcode.com/problems/palindrome-number/description/
+#
+# algorithms
+# Easy (41.61%)
+# Total Accepted:    545.5K
+# Total Submissions: 1.3M
+# Testcase Example:  '121'
+#
+# Determine whether an integer is a palindrome. An integer is a palindrome when
+# it reads the same backward as forward.
+# 
+# Example 1:
+# 
+# 
+# Input: 121
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it
+# becomes 121-. Therefore it is not a palindrome.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+# 
+# 
+# Follow up:
+# 
+# Coud you solve it without converting the integer to a string?
+# 
+#
+
+class Deque:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def addFront(self, item):
+        self.items.append(item)
+    def addRear(self, item):
+        self.items.insert(0,item)
+    def removeFront(self):
+        return self.items.pop()
+    def removeRear(self):
+        return self.items.pop(0)
+    def size(self):
+        return len(self.items)
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        chardeque = Deque()
+        for ch in str(x):
+            chardeque.addRear(ch)
+        stillEqual = True
+        while chardeque.size()>1 and stillEqual:
+            first = chardeque.removeFront()
+            last = chardeque.removeRear()
+            if first!=last:
+                stillEqual=False
+        return stillEqual
+
+        
+
