1f4a7f185e3bad9c26853614fbd6b4fe9297c27c
huiyang lu <luchocoice@gmail.com>
2019-04-13 23:52:24 +0800

    Update 20.valid-parentheses.py

--- LeetCode/20.valid-parentheses.py
+++ LeetCode/20.valid-parentheses.py
@@ -61,5 +61,15 @@
 #
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        if s == '':
+            return True
+        while True:
+            if ('()' in s) or ('{}' in s) or ('[]' in s):
+                s = s.replace('()','')
+                s = s.replace('{}','')
+                s = s.replace('[]','')
+                if s == '':
+                    return True
+            else:
+                return False
 
