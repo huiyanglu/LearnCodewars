9b3ee3d540313fabbcf1869e700da131e2e8b0fa
huiyang lu <luchocoice@gmail.com>
2019-04-22 23:57:47 +0800

    Create 38.count-and-say.py

---
+++ LeetCode/38.count-and-say.py
@@ -0,0 +1,60 @@
+#
+# @lc app=leetcode id=38 lang=python3
+#
+# [38] Count and Say
+#
+# https://leetcode.com/problems/count-and-say/description/
+#
+# algorithms
+# Easy (40.07%)
+# Total Accepted:    273.6K
+# Total Submissions: 681.9K
+# Testcase Example:  '1'
+#
+# The count-and-say sequence is the sequence of integers with the first five
+# terms as following:
+# 
+# 
+# 1.     1
+# 2.     11
+# 3.     21
+# 4.     1211
+# 5.     111221
+# 
+# 
+# 1 is read off as "one 1" or 11.
+# 11 is read off as "two 1s" or 21.
+# 21 is read off as "one 2, then one 1" or 1211.
+# 
+# Given an integer n where 1 ≤ n ≤ 30, generate the n^th term of the
+# count-and-say sequence.
+# 
+# Note: Each term of the sequence of integers will be represented as a
+# string.
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: 1
+# Output: "1"
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: 4
+# Output: "1211"
+# 
+#
+import re
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        for _ in range(n - 1):
+            s = ''.join(str(len(list(group))) + digit
+                        for digit, group in itertools.groupby(s))
+        return s
+
