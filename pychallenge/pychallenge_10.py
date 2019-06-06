295d59bf698bed4bfc88d7065765d9af76965670
huiyang lu <luchocoice@gmail.com>
2019-06-06 23:33:05 +0800

    Create pychallenge_10.py

---
+++ pychallenge/pychallenge_10.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: Huiyang Lu
+import itertools
+
+def ans(n: int) -> str:
+    s = '11'
+    for _ in range(n-1):
+        s = ''.join(str(len(list(group)))+digit for digit,group in itertools.groupby(s))
+    return len(s)
+
+print(ans(30))
